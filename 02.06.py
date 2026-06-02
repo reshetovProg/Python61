241f338b52020d201c81a6c33bb3774bbe8fc2bc
testName <test>
2026-06-02 21:08:06 +0300

    условия

---
+++ 02.06.py
@@ -0,0 +1,20 @@
+# разложение числа на разряды
+# num1 = int(input("введите число: "))
+#
+# d1 = num1%10
+# d2 = num1//10%10
+# d3 = num1//100%10
+# d4 = num1//1000
+# print((d1*1000+d2*100+d3*10+d4)*2)
+
+name = input("input your name: ")
+if name=="Ivan":
+    print("yes")
+elif name=="ivan":
+    print("yes")
+else:
+    print("no")
+
+
+
+
